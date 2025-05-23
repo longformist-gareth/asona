ac0130bceb75320bd3c127b3350f552e74488d98
Gareth <gareth@longformist.co.uk>
2025-05-23 09:56:20 +0100

    advertising ajax call

--- assets/upfront/js/custom.js
+++ assets/upfront/js/custom.js
@@ -12,20 +12,17 @@ function addPortalCustomization(){
 function addPromos(){
 	var sPromo = '';
 	if ($('.article-content').length) {
-
-	   $.ajax({
-	        url: 'https://theupfront.media/assets/upfront/ajax/ajax_standardPromo.htm'
-	        type: 'GET'
-	        data: ''
-	        dataType: 'html'
-	        success: function(res, status) {
-			$('.article-content p').each(function(index) {
+		$.ajax({
+	        url: 'https://theupfront.media/assets/upfront/ajax/ajax_standardPromo.htm',
+			cache: false
+		})
+		  .done(function( html ) {
+		    $('.article-content p').each(function(index) {
             	if(index == 0){
-            		$(this).after(res);
+            		$(this).after(html);
             		console.log('ads initialized');
             	}
             });
-	        }
-	    });
+		 });
 	};
 }
